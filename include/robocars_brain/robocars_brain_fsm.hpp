012ac257b72f0857a200dd4d9424edca4f6901fd
Benoit TRINITE <btrinite@free.fr>
2020-06-22 14:15:29 +0200

    doc

--- include/robocars_brain/robocars_brain_fsm.hpp
+++ include/robocars_brain/robocars_brain_fsm.hpp
@@ -1,3 +1,38 @@
+/**
+ * @file robocars_brain_fsm.hpp
+ * @brief Main car FSM, hold car global state.
+ * 
+ * Copyright (c) 2020 Benoit TRINITE
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ * Topic subscribed : 
+ *  - /radio_channels : to get command from radio controller
+ * 
+ * Topic published :
+ *  - /robocars_brain_state : to broadcast car state to any other ROS node
+ *  - /robocars_debug : to send debug message on ESP32 companion 
+ * 
+ * Parameters :
+ *  - mainIPInterface : the name of the main IP interface for which current IP address is reported in debug msg
+ */
+
 #include <tinyfsm.hpp>
 #include <ros/ros.h>
 #include <stdio.h>
